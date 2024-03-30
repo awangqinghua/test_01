dc54baf0dcaa5c275b6350ede25d6dd52c7c8738
huahua <62108326+awangqinghua@users.noreply.github.com>
2024-03-30 11:04:44 +0800

    Add files via upload

---
+++ test_001/test_001/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# @Time     : 2024-03-30 11:03
+# @Author   : 华
+# @Email    : 867075698@qq.com
+# @File     : __init__.py.py
+# @Software : PyCharm
---
+++ test_001/test_001/test000.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# @Time     : 2024-03-30 11:03
+# @Author   : 华
+# @Email    : 867075698@qq.com
+# @File     : test000.py
+# @Software : PyCharm
+
+
+
+print('hello,world')
